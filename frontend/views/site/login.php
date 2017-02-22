74b9437838d97500f035d93b3c973d011bf01568
Yuri <yurakorenyk@gmail.com>
2017-02-22 23:00:28 +0300

    Fix 3. Форма входа для организаторов. Убраны поля регистрации.

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -11,6 +11,6 @@ $this->title = 'Вход для организаторов';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1 style="text-align: center"><?= $this->title?></h1>
-<h2 class="text-center">Выберите удобную социальную сеть</h2>
+<h2 class="text-center login__sub-header">Выберите удобную социальную сеть для входа</h2>
 <p style="text-align: center;"><a href="/site/auth?authclient=vkontakte"><img src="/img/vk.png" /></a> <a href="/site/auth?authclient=facebook"><img src="/img/fb.png" /></a> <a href="/site/auth?authclient=twitter"><img src="/img/tw.png" /></a></p>
 
